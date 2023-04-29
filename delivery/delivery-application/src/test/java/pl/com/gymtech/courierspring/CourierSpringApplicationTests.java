d81817716340a0eb75319819469cc3172455be78
Jacek Pozowski <jacekpozo@gmail.com>
2023-04-29 14:06:04 +0200

    Initial commit. Creating Project structure and basic dependencies.

---
+++ delivery/delivery-application/src/test/java/pl/com/gymtech/courierspring/CourierSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.gymtech.courierspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ delivery/delivery-infrastructure/src/main/java/pl/com/gymtech/courierspring/CourierSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.com.gymtech.courierspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourierSpringApplication.class, args);
+    }
+
+}
