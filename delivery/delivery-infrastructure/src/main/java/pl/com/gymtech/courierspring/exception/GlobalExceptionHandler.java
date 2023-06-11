ce0c803c8d07d483561c6c4118d9764826dac87a
Jacek Pozowski <jacekpozo@gmail.com>
2023-06-11 18:46:46 +0200

    Adding optimisticlock handling

--- delivery/delivery-infrastructure/src/main/java/pl/com/gymtech/courierspring/exception/GlobalExceptionHandler.java
+++ delivery/delivery-infrastructure/src/main/java/pl/com/gymtech/courierspring/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package pl.com.gymtech.courierspring.exception;
 import org.flywaydb.core.api.ErrorDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -32,4 +33,14 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
+    @ExceptionHandler(ObjectOptimisticLockingFailureException.class)
+    public ResponseEntity<Error> handleOptimisticLockException(ObjectOptimisticLockingFailureException ex){
+        Error errorDetails= new Error();
+        errorDetails.setCode(HttpStatus.PRECONDITION_FAILED.value());
+        errorDetails.setMessage("The resource has been updated in the meantime!");
+        errorDetails.setDetails(ex.getMessage());
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.PRECONDITION_FAILED);
+    }
+
 }
